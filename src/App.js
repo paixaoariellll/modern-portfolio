b7be162ed44ffeb0439bba7f967ec641380e23d4
paixaoariellll <paixaoariellll@gmail.com>
2022-08-23 10:08:58 -0300

    Header navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
-  return (
-    <h1>Hello world</h1>
-  );
+  return <>
+    <Header />
+  </>
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,59 @@
 import React from "react";
+import "./Header.css";
+
+const nav_links = [
+  {
+    path: "#home",
+    display: "Home",
+  },
+  {
+    path: "#about",
+    display: "About",
+  },
+  {
+    path: "#service",
+    display: "Service",
+  },
+  {
+    path: "#projects",
+    display: "Projects",
+  },
+  {
+    path: "#contact",
+    display: "Contact",
+  },
+];
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="container"></div>
+      <div className="container">
+        <div className="nav_bar">
+          <div className="logo">
+            <h2>Ariel Paixão</h2>
+          </div>
+
+          {/* ===== nvegação ===== */}
+          <div className="navigation">
+            <ul className="menu">
+              {nav_links.map((item, index) => (
+                <li className="menu_item">
+                  <a href={item.path} className="menu_index">
+                    {item.display}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* ===== ligth mode ===== */}
+          <div className="light_mode">
+            <span>
+              <i class="ri-sun-line"></i> Light Mode
+            </span>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
